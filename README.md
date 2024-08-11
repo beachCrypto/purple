c3d9f05f4f1684cb6dc321c2318967c903f77734
Beach Crypto <beachcrypto1@gmail.com>
2024-08-11 00:52:36 -0700

    purple frames

--- README.md
+++ README.md
@@ -67,5 +67,3 @@ After doing this you can run the `dev` or `preview` script and visit the `/api/h
 
 Finally, if you also want to see the example work in the deployed application make sure to add a `MY_KV_NAMESPACE` binding to your Pages application in its [dashboard kv bindings settings section](https://dash.cloudflare.com/?to=/:account/pages/view/:pages-project/settings/functions#kv_namespace_bindings_section). After having configured it make sure to re-deploy your application.
 # next-frog-cloudflare
-# purple
-# purple
---
+++ src/app/api/[[...routes]]/route.tsx
@@ -0,0 +1,99 @@
+/* eslint-disable react/jsx-key */
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog';
+import { devtools } from 'frog/dev';
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next';
+import { serveStatic } from 'frog/serve-static';
+import { env } from 'process';
+
+export interface Env {
+  AIRSTACK_API_TOKEN: string;
+}
+
+const app = new Frog({
+  hub: {
+    apiUrl: 'https://hubs.airstack.xyz',
+    fetchOptions: {
+      headers: {
+        'x-airstack-hubs': env.AIRSTACK_API_TOKEN ?? '',
+      },
+    },
+  },
+  title: 'Purple Frames',
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+});
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+export const runtime = 'edge';
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c;
+  const fruit = inputText || buttonValue;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'purple',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Purple
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Link href="https://nouns.build/dao/base/0x8de71d80eE2C4700bC9D4F8031a2504Ca93f7088">
+        Share
+      </Button.Link>,
+      ,
+      <Button.Link href="https://nouns.build/dao/base/0x8de71d80eE2C4700bC9D4F8031a2504Ca93f7088">
+        Auction
+      </Button.Link>,
+    ],
+  });
+});
+
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
+// static assets to the public folder. You can do this by adding a script to your package.json:
+// ```json
+// {
+//   scripts: {
+//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
+//   }
+// }
+// ```
+// Next, you'll want to set up the devtools to use the correct assets path:
+// ```ts
+// devtools(app, { assetsPath: '/.frog' })
+// ```
