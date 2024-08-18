e58116da6730bacde9415ae2125601e939cf81e3
Beach Crypto <beachcrypto1@gmail.com>
2024-08-18 13:49:40 -0700

    place bid transaction is working on testnet

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-key */
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog';
+import { Button, Frog, TextInput, parseEther } from 'frog';
 import { devtools } from 'frog/dev';
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next';
@@ -112,16 +112,78 @@ app.frame('/more', (c) => {
       </div>
     ),
     intents: [
+      <Button action={`/join`}>Join</Button>,
       <Button.Link href="https://warpcast.com/~/compose?embeds%5B%5D=https%3A%2F%2Fpurple-frames.pages.dev%2Fapi&text=get+purple+pilled+-+frame+by+%40pichi+and+%40beachcrypto">
         Share
       </Button.Link>,
-      ,
       <Button.Link href="https://nouns.build/dao/base/0x8de71d80ee2c4700bc9d4f8031a2504ca93f7088?referral=0x83f2af0F0aC4412F118B31f7dd596309B25b34Dd">
         Auction
       </Button.Link>,
     ],
   });
 });
+
+app.frame('/join', (c) => {
+  const { buttonValue, inputText, status } = c;
+  const fruit = inputText || buttonValue;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'purple',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Purple’s goal is to proliferate and expand the Farcaster
+          protocol and ecosystem.
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Value (ETH)" />,
+      <Button.Transaction target={`/mint`}>Mint</Button.Transaction>,
+      <Button action={`/`}>Back</Button>,
+    ],
+  });
+});
+
+let auctionABI = require('../../../abi/abi.json');
+
+console.log('abi', auctionABI);
+
+app.transaction('/mint', (c) => {
+  // Contract transaction response.
+  return c.contract({
+    abi: auctionABI,
+    chainId: 'eip155:84532',
+    functionName: 'createBid',
+    value: parseEther('.001'),
+    args: [11],
+    to: '0x03855976fcb91bf23110e2c425dcfb1ba0635b79',
+  });
+});
+
+// token id - 610
+// referral - 0x83f2af0F0aC4412F118B31f7dd596309B25b34Dd
 
 devtools(app, { serveStatic });
 
